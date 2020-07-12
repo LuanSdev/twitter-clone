0a3d0db35e77734658591b17d6504e8ae8c66a08
indominuShot <luang193@gmail.com>
2020-07-12 00:24:19 -0300

    --fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import GlobalStyles from './styles/GloblaStyles';
 
+import Layout from './Components/Layout';
+
 function App() {
   return (
     <>
+      <Layout />
       <GlobalStyles />
-      <div className="App"></div>
     </>
   );
 }
